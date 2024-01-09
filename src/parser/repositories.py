e06e04355a3d4bc695c1a0ad048465d5f32219c0
NistsiukAlexandr <nistsiuk.aliaksandr@pynest.io>
2024-01-09 11:05:14 +0300

    fix: delete db collection call

--- src/parser/repositories.py
+++ src/parser/repositories.py
@@ -6,18 +6,18 @@ from src.interfaces import IRepository
 
 class MongoDBRepository(IRepository):
     async def insert_one(self, document: dict):
-        return await db_collection().insert_one(document)
+        return await db_collection.insert_one(document)
 
     async def insert_many(self, documents: Iterable[dict]):
-        return await db_collection().insert_many(documents)
+        return await db_collection.insert_many(documents)
 
     async def filter(self, filters: dict):
-        result = db_collection().find(filters)
+        result = db_collection.find(filters)
 
         return [document async for document in result]
 
     async def get_one(self, filters: dict):
-        return await db_collection().find_one(filters)
+        return await db_collection.find_one(filters)
 
     async def get_all(self):
         return await self.filter({})
