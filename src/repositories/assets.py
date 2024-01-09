016c25476674227eef6e7f4504505dd3efb1e3e5
NistsiukAlexandr <nistsiuk.aliaksandr@pynest.io>
2024-01-09 22:08:12 +0300

    feat: add get_group_by_assets method

--- src/repositories/assets.py
+++ src/repositories/assets.py
@@ -4,3 +4,10 @@ from repositories.base import BaseRepository
 
 class AssetRepository(BaseRepository):
     collection = get_settings().mongodb_collection
+
+    async def get_group_by_assets(self):
+        return (
+            await self.db[self.collection]
+            .aggregate([{"$group": {"_id": "$symbol", "values": {"$push": "$price"}}}])
+            .to_list(length=None)
+        )
