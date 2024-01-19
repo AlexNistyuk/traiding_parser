91d0095ced0fa435b6b1ada253be525813473b3f
NistsiukAlexandr <nistsiuk.aliaksandr@pynest.io>
2024-01-19 12:01:01 +0300

    feat: add exceptions handling, add status codes

--- src/presentation/api/v1/coins.py
+++ src/presentation/api/v1/coins.py
@@ -1,12 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 from application.use_case.coins import CoinUseCase
 
 router = APIRouter(prefix="/coins", tags=["Update coins"])
 
 
-@router.get("/update", response_model=dict)
+@router.get("/update", status_code=HTTP_200_OK)
 async def update_coins():
-    await CoinUseCase().get_data_and_send()
+    try:
+        await CoinUseCase().get_data_and_send()
 
-    return {"detail": "successfully"}
+        return JSONResponse({"detail": "successfully"})
+    except Exception as exc:
+        raise HTTPException(
+            detail={"error": str(exc)}, status_code=HTTP_400_BAD_REQUEST
+        )
