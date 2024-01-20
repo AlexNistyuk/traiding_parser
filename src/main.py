ac709bc0d89c7a938585df22f464ff8e9d0fb542
NistsiukAlexandr <nistsiuk.aliaksandr@pynest.io>
2024-01-20 19:57:05 +0300

    feat: delete debug engine

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from contextlib import asynccontextmanager
 
-import uvicorn
 from fastapi import FastAPI
 
 from infrastructure.managers.binances import BinanceManager
@@ -25,7 +24,3 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(v1_router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app=app)
